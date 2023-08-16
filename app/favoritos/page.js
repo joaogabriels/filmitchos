a4455b3f00d588e1cb7b1422f4fdde45c8079462
João Evangelista <joaogabrielsevangelista@gmail.com>
2023-08-16 02:07:56 -0300

    feat(clerk): adiciona base para autenticação do app

--- app/favoritos/page.js
+++ app/favoritos/page.js
@@ -1,4 +1,12 @@
+'use client';
+
+import { useUser } from "@clerk/nextjs";
+
 export default function Favoritos() {
+  const { isSignedIn, isLoading, user } = useUser()
+
+  console.log(isSignedIn, isLoading, user);
+
   return (
     <div>
       Favoritos
--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 
 import Header from '@/components/Header'
+import { ClerkProvider } from '@clerk/nextjs'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,12 +13,14 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="pt-BR">
-      <body className={inter.className}>
-        <Header />
+    <ClerkProvider>
+      <html lang="pt-BR">
+        <body className={inter.className}>
+          <Header />
 
-        {children}
-      </body>
-    </html>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
