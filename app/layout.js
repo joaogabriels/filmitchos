16497bf83b84c5503ac2e0f72df2f8a6e5e0561e
joaogabriels <joaogabrielsevangelista@gmail.com>
2023-08-22 21:46:24 -0300

    refactor(layout): remove provider do clerkjs

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,4 @@
 import { Inter } from 'next/font/google'
-import { ClerkProvider } from '@clerk/nextjs'
 
 import Header from '@/components/Header'
 import { ThemeProvider } from "@/components/ThemeProvider"
@@ -15,18 +14,16 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
-      <html lang="pt-BR">
-        <body className={inter.className}>
-          <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
-            <div className="container pt-4 pb-4">
-              <Header />
+    <html lang="pt-BR">
+      <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+          <div className="container pt-4 pb-4">
+            <Header />
 
-              {children}
-            </div>
-          </ThemeProvider>
-        </body>
-      </html>
-    </ClerkProvider>
+            {children}
+          </div>
+        </ThemeProvider>
+      </body>
+    </html>
   )
 }
